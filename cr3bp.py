6d573ebe89a156e3aab921b7c3438f840cb3db34
Maxwell Zweig <mazy2@cornell.edu>
2025-03-03 22:12:09 -0500

    adding optimizer code

--- cr3bp.py
+++ cr3bp.py
@@ -31,3 +31,49 @@ class CR3BP_Thrust_Dynamics(oc.ODEBase):
         ode = vf.stack([xdot,ydot,zdot,xddot,yddot,zddot])
         ####################################################
         super().__init__(ode,Xvars,Uvars)
+
+
+def run_optimizer(ode, IG, BoundaryFirst, BoundaryLast, optType, E_or_T, Umax, Umin, numKnots, numThreads, MeshTol, EControl):
+    phase = ode.phase(optType, IG, numKnots)
+    #Fix first state and time
+    phase.addBoundaryValue("First", range(0,7), BoundaryFirst) #Q: Does this have constraints on the dimensionality? A: 1d array if this doesnt work
+    #Fix last state and time
+    phase.addBoundaryValue("Last", range(0,7), BoundaryLast)
+    # Bound control forces
+    phase.addLUNormBound("Path",[7,8,9],Umin,Umax) #Q: how to write this line? A: LUNorm - don't let it go to 0
+    #Produce a mass-optimal result by integrating over a norm() applied to the thrust vector
+    if E_or_T == 0:
+        phase.addIntegralObjective(Args(3).squared_norm(),[7,8,9]) 
+    else:
+        phase.addIntegralObjective(Args(3).norm(),[7,8,9])
+
+    
+    # here, we are adding adaptive meshing
+    phase.setAdaptiveMesh(True)  #Enable Adaptive mesh for all following solve/optimize calls
+    #phase.setMeshErrorEstimator('deboor')     #default
+    ## Set Error tolerance on mesh (epsilon)
+    phase.setMeshTol(MeshTol)  #default = 1.0e-6 
+    ## Make sure to set optimizer EContol to be the same as or smaller than MeshTol
+    phase.optimizer.set_EContol(EControl)
+    ## Set Max number of mesh iterations:
+    #phase.setMaxMeshIters(10)  #default = 10
+    
+    phase.setThreads(numThreads,numThreads) #Q: what does this do? A:Parallelization
+    phase.optimizer.set_PrintLevel(1)
+    phase.optimize()
+
+    return phase
+
+if __name__ == "__main__":
+
+    mu_star =  0.01215059   # Constant for CR3BP
+    Umax = 0.0184*100           # DU/TU^2
+
+
+    # REFERENCE TRAJECTORY STATE AND PERIOD
+    tf  = 2.085034838884136 # TU
+    # Load in the reference state data
+    FileName_ref = "C:/Users/ccm41/Downloads/Research/Starlift/STM_Stuff/STMPrecompute-main/STMPrecompute-main/EnergyOptimal_state_EarthMoon_L2nrho.mat"
+    ref_state = list(scipy.io.loadmat(FileName_ref).values())[-1]
+
+    
